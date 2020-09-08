72eb268d78abbf041757ad0cbde8876149a02b8f
Greg Mall <gregmall157@gmail.com>
2020-09-08 14:10:43 -0700

    first two tests on fist model

---
+++ sql/setup.sql
@@ -0,0 +1,8 @@
+DROP TABLE IF EXISTS beers;
+
+CREATE TABLE beers (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    name TEXT NOT NULL,
+    type TEXT CHECK (type = ANY('{lager,pilser,ale}')),
+    cost INT CHECK (cost > 0)
+);
