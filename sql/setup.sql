105179a5ef8835d24069e9211fffb56e227983b8
Greg Mall <gregmall157@gmail.com>
2020-09-08 14:49:55 -0700

    last test on first model

--- sql/setup.sql
+++ sql/setup.sql
@@ -3,6 +3,6 @@ DROP TABLE IF EXISTS beers;
 CREATE TABLE beers (
     id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     name TEXT NOT NULL,
-    type TEXT CHECK (type = ANY('{lager,pilser,ale}')),
+    type TEXT CHECK (type = ANY('{lager,pilsner,ale}')),
     cost INT CHECK (cost > 0)
 );
