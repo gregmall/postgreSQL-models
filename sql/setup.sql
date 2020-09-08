72c57347fee232cb181d0b45728f76e541b8870c
Greg Mall <gregmall157@gmail.com>
2020-09-08 15:38:29 -0700

    second model and first test

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,4 +1,5 @@
 DROP TABLE IF EXISTS beers;
+DROP TABLE IF EXISTS guitars;
 
 CREATE TABLE beers (
     id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
@@ -6,3 +7,11 @@ CREATE TABLE beers (
     type TEXT CHECK (type = ANY('{lager,pilsner,ale}')),
     cost INT CHECK (cost > 0)
 );
+
+CREATE TABLE guitars (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    name TEXT NOT NULL,
+    type TEXT CHECK (type = ANY('{guitar,bass}')),
+    cost INT CHECK (cost > 0)
+);
+
