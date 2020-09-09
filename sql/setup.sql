0e013df49291100568ff986da1352fefd1501ec7
Greg Mall <gregmall157@gmail.com>
2020-09-08 19:32:09 -0700

    fourth model and tests completed

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,6 +1,7 @@
 DROP TABLE IF EXISTS beers;
 DROP TABLE IF EXISTS guitars;
 DROP TABLE IF EXISTS shoes;
+DROP TABLE IF EXISTS cars;
 
 CREATE TABLE beers (
     id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
@@ -22,4 +23,12 @@ CREATE TABLE shoes (
     type TEXT CHECK (type = ANY('{running, basketball, track, golf}')),
     cost INT CHECK (cost > 0)
 );
+
+CREATE TABLE cars (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    name TEXT NOT NULL,
+    type TEXT CHECK (type = ANY('{car, truck, SUV}')),
+    cost INT CHECK (cost > 0)
+);
+
 
